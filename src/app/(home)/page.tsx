f00ccf9f1ed8f5d094a6b3b317e984bff6944044
RadidDesafandri <radietdesfandri@gmail.com>
2025-03-10 11:24:41 +0700

    setup project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-montserrat flex h-full w-full items-center justify-center p-5 text-5xl">
+      ASDFGHJ12345678910
+    </div>
+  );
+}
